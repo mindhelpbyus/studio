6b12fbc90a5132477b854097324e7da50f858760
Vignesh Prabu <mindhelpbyus@gmail.com>
2025-09-17 01:34:28 +0000

    fix the error, applicaion is not starting

---
+++ tailwind.config.mjs
@@ -0,0 +1,59 @@
+import designTokens from './src/lib/design-tokens.json';
+
+const {colors, fontFamily, borderRadius, boxShadow} = designTokens;
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        body: ["var(--font-manrope)", "sans-serif"],
+        headline: ["var(--font-manrope)", "sans-serif"],
+      },
+      colors,
+      borderRadius: {
+        ...borderRadius,
+        full: "9999px",
+      },
+      boxShadow,
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+};
+
+export default config;
